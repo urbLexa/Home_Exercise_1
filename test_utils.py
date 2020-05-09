208ab4eeae3f8f31c8860a86a3cabdbf54927386
axel <ax.bruck@gmail.com>
2020-05-09 12:35:20 +0100

    Test file
    Test the pv_Power function

---
+++ test_utils.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  9 12:10:44 2020
+
+@author: Axel
+"""
+
+from utils import pv_Power
+
+
+'''
+Test panel: https://static.trinasolar.com/sites/default/files/Datasheet_Tallmax_1500V%20M%20Plus_2019_May.pdf
+Test irradiance on tilted surface 600 W/m2
+Roof dimensions 6 x 15 meters
+'''
+panel = {
+    "x_pan": 0.992,
+    "y_pan": 1.960,
+    "b": 0.04,
+    "eff": 0.195
+    }
+
+g_tilt = 600
+
+def test_pv_Power():
+    assert pv_Power(g_tilt, 6, 15, panel) == 6.740278272
